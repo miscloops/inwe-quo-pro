e1f3832a3e403e26ce5e7a094848435f769c74b3
miscloops <miscloops@gmail.com>
2026-08-26 04:30:02 +0500

    Add webpack configuration for Cloudflare Workers

--- next.config.ts
+++ next.config.ts
@@ -2,11 +2,17 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "standalone",
-  /* config options here */
   typescript: {
     ignoreBuildErrors: true,
   },
   reactStrictMode: false,
+  webpack: (config, { isServer }) => {
+    if (isServer) {
+      config.externals = config.externals || [];
+      config.externals.push("cloudflare:workers");
+    }
+    return config;
+  },
 };
 
 export default nextConfig;
